b12470d1a9d23d97dabe40b6621a321f7cd70b9b
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-27 23:40:23 +0200

    Connection to MongoDB

---
+++ config/connectDB.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI, {});
+
+        console.log(`MongoDB Connected to ${connect.connection.host}`);
+    } catch (err) {
+        console.log(err)
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const connectDB = require('./config/connectDB');
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 4500;
+
+connectDB();
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
