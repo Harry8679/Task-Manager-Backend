81a4914ff7014a7996266f3bc1f000ffa3ba3763
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-28 13:08:12 +0200

    Refactor our routes

--- routes/task.route.js
+++ routes/task.route.js
@@ -2,10 +2,13 @@ const express = require('express');
 const { getAllTasks, createTask, SingleTask, DeleteTask, updateTask } = require('../controllers/task.controller');
 const router = express.Router();
 
-router.get('/', getAllTasks);
-router.get('/:id', SingleTask);
-router.post('/create', createTask);
-router.delete('/:id', DeleteTask);
-router.put('/:id', updateTask);
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(SingleTask).delete(DeleteTask).put(updateTask);
+
+// router.get('/', getAllTasks);
+// router.get('/:id', SingleTask);
+// router.post('/create', createTask);
+// router.delete('/:id', DeleteTask);
+// router.put('/:id', updateTask);
 
 module.exports = router;
