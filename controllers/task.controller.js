3df3d683675be93f96f7e6b3dcb24bb543029de1
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-28 11:43:56 +0200

    AsyncHandler library installed

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,5 +1,7 @@
-const getAllTasks = (req, res) => {
-    res.send('Get All Tasks updated');
-}
+const asyncHandler = require('express-async-handler');
+
+const getAllTasks = asyncHandler(async (req, res) => {
+    res.send('Get All Tasks updated 2');
+});
 
 module.exports = { getAllTasks };
