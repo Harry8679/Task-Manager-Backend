ca9fabf1db7acbc8f8e8f1d5cdf3dfee3b1ef543
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-07 23:29:35 +0200

    server file updated

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-// const mongoose = require('mongoose');
 const connectDB = require('./config/connectDB');
 const cors = require('cors');
 
@@ -10,16 +9,22 @@ dotenv.config();
 
 const app = express();
 
+// Utiliser le middleware CORS pour autoriser les requêtes cross-origin
+app.use(cors({
+    origin: 'http://localhost:3003', // Remplace par l'URL de ton frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Spécifie les méthodes autorisées
+    credentials: true, // Si tu utilises des cookies ou des informations d'authentification
+}));
+
 app.use(express.json());
 app.use('/api/v1/tasks', taskRoutes);
-app.use(cors());
 
 const PORT = process.env.PORT || 4500;
 
+// Connexion à la base de données
 connectDB();
 
-
-
+// Démarrer le serveur
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
-})
+});
